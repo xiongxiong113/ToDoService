959f6632be64b69a217ddefe26cf9f753c87d73e
Jiahui Xiong <jxiong5@slb.com>
2022-11-07 20:07:08 +0800

    feat: change API to true API

--- ToDoFrontEnd/src/app/api/todo.api.service.ts
+++ ToDoFrontEnd/src/app/api/todo.api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ToDoItem } from '../model/ToDoItem';
-
+const baseUrl='https://localhost:5001/'
 @Injectable({
   providedIn: 'root',
 })
@@ -10,6 +10,6 @@ export class TodoApiService {
   constructor(private http: HttpClient) {}
 
   create(todoitem: ToDoItem): Observable<void>{
-    return this.http.post<void>('https://635fc244ca0fe3c21aa3d012.mockapi.io/api/todos', todoitem)
+    return this.http.post<void>(`${baseUrl}todos`, todoitem)
   }
 }
